885a0cd411b20415a1dd9bdcd342ae549948e0a9
Pavel Lebedev <pavel.o.lebdev@gmail.com>
2022-07-14 11:07:52 +0300

    fix naming

--- src/hooks/useArticlesList.ts
+++ src/hooks/useArticlesList.ts
@@ -2,13 +2,13 @@ import { ArticleListItemType } from 'models/articles-list-item';
 import { articlesListFetcher } from 'service/articlesListFetcher';
 import useSWR from 'swr';
 
-type UseStories = {
+type UseArticlesList = {
   articles: ArticleListItemType[];
   isLoading: boolean;
   isError: boolean;
 };
 
-export const useArticlesList = (section: string): UseStories => {
+export const useArticlesList = (section: string): UseArticlesList => {
   const { data, error } = useSWR(section, articlesListFetcher);
 
   return {
